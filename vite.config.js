542b37e15ec7ec3bfcc637af29e9c747b0629a46
imcasero <99729923+imcasero@users.noreply.github.com>
2023-06-29 20:04:12 +0200

    deploy and fav

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "Likes-page", // add the base as repo name as "/yourRepoName/"
   plugins: [react()],
-})
+});
